1886f3f820e29466c544ba765acca028ec1ce29d
yuxin670 <107701554+yuxin670@users.noreply.github.com>
2022-10-14 22:09:24 -0400

    Initial Commit

---
+++ Parstagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Parstagram
+//
+//  Created by yuxin Chen on 10/14/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
