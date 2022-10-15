dd0f2c05905febfef00103e6f20ab3cf5b4b79d6
yuxin670 <107701554+yuxin670@users.noreply.github.com>
2022-10-15 03:02:46 -0400

    first commit
    implemented the first part of the assignment

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                $0.applicationId = "H3F7HErKZgWbSG1FrhNAVmT50p9Lbfe1o8AyIEMt" // <- UPDATE
+                $0.clientKey = "WkmtfR1oYvkWWxt3JFMmEzyPgHXKsynXBNzdkkzS" // <- UPDATE
+                $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: parseConfig)
         return true
     }
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Parstagram' do
+
+ 	# Add Parse pods and install
+  use_frameworks!
+ 	pod 'Parse'
+ 	pod 'AlamofireImage'
+end
